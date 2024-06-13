de088cbc8f7079a1c4815acf1e8363a7a6812435
Nirali <nirali.mehta112@gmail.com>
2024-06-13 00:20:08 -0700

    add one more entries in each data models and fixed initDB.js file

--- initDB.js
+++ initDB.js
@@ -6,14 +6,18 @@ require("./model/relationship");
 const assignmentData = require("./data/Assignments.json");
 const coursesData = require("./data/Courses.json");
 const usersData = require("./data/Users.json");
-
 const sequelize = require("./lib/sequelize");
 
 // Correct usage of sequelize.sync()
 sequelize.sync().then(async function () {
-  await Courses.bulkCreate(coursesData);
-  await User.bulkCreate(usersData);
-  await Assignments.bulkCreate(assignmentData);
+  try {
+    await Courses.bulkCreate(coursesData);
+    await User.bulkCreate(usersData);
+    await Assignments.bulkCreate(assignmentData);
+    console.log("Database has been initialized successfully.");
+  } catch (error) {
+    console.error('Error during database synchronization:', error);
+  }
 }).catch(error => {
   console.error('Error during database synchronization:', error);
 });
