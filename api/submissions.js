22830c1e1833227802faa87b0c8694a8b1461438
Hao Jun Chen <100190491+RayHao2@users.noreply.github.com>
2024-06-14 08:44:10 -0700

    patch submissions auth done

--- api/submissions.js
+++ api/submissions.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 const path = require("path");
+const { Assignments } = require("../model/assignments");
+const { Courses } = require("../model/courses");
 const router = Router();
 const { Submissions, submissionFields } = require("../model/submissions");
-
-router.patch("/:submissionId", async (req, res) => {
+const auth = require("../lib/auth")
+router.patch("/:submissionId", auth.requireAuthentication, async (req, res) => {
   const { submissionId } = req.params;
   const updateData = req.body;
-
+  const { role: inputRole, user: userId } = req; 
   // Remove filePath from update data if it exists to ensure it is not changed
   if (updateData.filePath) {
     delete updateData.filePath;
@@ -18,6 +20,26 @@ router.patch("/:submissionId", async (req, res) => {
     if (!submission) {
       return res.status(404).json({ message: "Submission not found" });
     }
+
+    if (inputRole === "student" || !inputRole) {
+      return res.status(403).json({
+        message:
+          "Not authorized to update assignments, student role is giving ",
+      });
+    }
+    if (inputRole === "instructor") {
+      const assignment = await Assignments.findByPk(submission.assignmentId)
+      const course = await Courses.findOne({
+        where: { courseId: assignment.courseId, instructorId: userId },
+      });
+      if (!course) {
+        return res.status(403).json({
+          message:
+            "Not authorized to update this assignment, not the instructor in this class",
+        });
+      }
+    }
+
 
     // Update the submission
     const updatedSubmission = await submission.update(updateData);
